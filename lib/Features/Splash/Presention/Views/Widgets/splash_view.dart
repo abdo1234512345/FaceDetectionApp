43269703bfbbf1da413aacb599470ce0078d33a1
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-02-16 17:37:01 +0200

    create splash view

---
+++ lib/Features/Splash/Presention/Views/Widgets/splash_view.dart
@@ -0,0 +1,12 @@
+import 'package:facedetection/Features/Splash/Presention/Views/Widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
