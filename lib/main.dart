fb081aabd9fa33c1b2cd042bc78b7fd6584493b3
AbdelRahman Amr <abdoanonymous1221@gmail.com>
2025-02-16 17:36:32 +0200

    Create Main

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:facedetection/Features/Splash/Presention/Views/Widgets/splash_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+      darkTheme: ThemeData.dark(),
+    );
+  }
+}
